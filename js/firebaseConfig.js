d55c266052316143deb5e4fbf13fa773dd3c33ab
tfbbueno <tfbbueno@users.noreply.github.com>
2025-06-12 19:50:30 +0000

    deploy: 339b3dc958ceba75a90838f55ee047f9247b8c43

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+/* --------- GERADO AUTOMATICAMENTE pelo GitHub Actions --------- */
+export const firebaseConfig = {
+  apiKey: "AIzaSyAS7HBoKAlxTCK4pIifG76bP7JRLv6XnYw",
+  authDomain: "queeriosity-jogo-memoria-8d5d5.firebaseapp.com",
+  projectId: "queeriosity-jogo-memoria-8d5d5",
+  storageBucket: "queeriosity-jogo-memoria-8d5d5.firebasestorage.app",
+  messagingSenderId: "355143596461",
+  appId: "1:355143596461:web:5bb21b37f55f557f0c4924"
+};
+/* ---------------------------------------------------------------- */
